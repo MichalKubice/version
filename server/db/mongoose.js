b0d404fb32d70ac1f921bd9a578801d43e147d94
MichalKubice <38439841+MichalKubice@users.noreply.github.com>
2018-12-02 17:25:34 +0100

    UP

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<mike26>:<mike26>@ds123584.mlab.com:23584/todo_testing");
+mongoose.connect("mongodb://<mike>:<mike26>@ds123584.mlab.com:23584/todo_testing");
 
 module.exports = {mongoose};
