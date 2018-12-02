89663893d6a6081b78bdf4c277a2fd74ca7c3c31
MichalKubice <38439841+MichalKubice@users.noreply.github.com>
2018-12-02 17:19:50 +0100

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 var {mongoose} = require("./db/mongoose")
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {ObjectID} = require("mongodb");
 
 var express = require("express");
 var bodyParser = require("body-parser");
 
 
 var app = express();
-
+var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-
+// NEW TODO
 app.post("/todos", (req,res)=> {
   var todo = new Todo({
     text: req.body.text
@@ -25,7 +26,37 @@ app.post("/todos", (req,res)=> {
   });
 });
 
-app.listen(3000, ()=> {
-
-  console.log("Started on port 3000");
+app.get("/todos",(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (e)=> {
+    res.status(400).send(e);
+  })
 });
+
+app.get("/todos/:id", (req,res)=> {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+
+});
+
+app.listen(port, ()=> {
+
+  console.log(`Started on ${port}`);
+});
+
+
+module.exports = {app};
